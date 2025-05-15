0f6f52347dd547ac350be838d66e427878ac800f
janinynobrega <janinynobrega@gmail.com>
2025-05-15 13:32:10 -0300

    inicio da refatoração do primeiro carousel da  homepage

--- src/components/CarouselJ.jsx
+++ src/components/CarouselJ.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { CarouselJ } from 'antd';
 
-const CarouselJ = () => {
-  return (
-    <div>CarouselJ</div>
-  )
-}
-
-export default CarouselJ
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+const App = () => (
+  <CarouselJ autoplay={{ dotDuration: true }} autoplaySpeed={5000}>
+    <div>
+      <h3 style={contentStyle}>1</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>2</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>3</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>4</h3>
+    </div>
+  </CarouselJ>
+);
+export default App;
