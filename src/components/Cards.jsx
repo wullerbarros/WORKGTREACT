2dbf5cc39574037e534219c91412640942c68318
jonathas <jonathas.sabino@gmail.com>
2025-05-15 17:12:32 -0300

    Cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import tenis from "../assets/tenis.jpg"
 
-const cards = () => {
+
+
+const produtos = [
+  {
+      produto: "K-Swiss V8 - Masculino" ,
+      categoria: "Tenis",
+      preco: 100,
+      img: tenis
+  },{
+      produto: "K-Swiss V8 - Masculino",
+      categoria: "Tenis",
+      preco: 100,
+      img: tenis
+  },{
+      produto: "K-Swiss V8 - Masculino",
+      categoria: "Tenis",
+      preco: 100,
+      img: tenis
+  },{
+      produto: "K-Swiss V8 - Masculino",
+      categoria: "Tenis",
+      preco: 100,
+      img: tenis
+  },{
+    produto: "K-Swiss V8 - Masculino",
+    categoria: "Tenis",
+    preco: 100,
+    img: tenis
+},{
+  produto: "K-Swiss V8 - Masculino",
+  categoria: "Tenis",
+  preco: 100,
+  img: tenis
+},{
+  produto: "K-Swiss V8 - Masculino",
+  categoria: "Tenis",
+  preco: 100,
+  img: tenis
+},{
+  produto: "K-Swiss V8 - Masculino",
+  categoria: "Tenis",
+  preco: 100,
+  img: tenis
+}
+]
+
+
+const Cards = () => {
   return (
-    <div>cards</div>
+    <>
+    <section className="ml-[100px]">
+            <h1 className="font-bold text-2xl mb-5">Produtos Relacionados</h1>
+              <ul className="flex flex-wrap gap-[24px]">
+                {produtos.map((item)=>(
+                    <li className="border border-[#ccc] p-2">
+                        <div className="bg-[#E2E3FF] h-28 w-32 flex items-center justify-center rounded-sm">
+                            <img src={item.img} alt={item.produto} />
+                        </div>
+                        <div>
+                            <h2>{item.categoria}</h2>
+                            <h2 className="text-xs">{item.produto}</h2>
+                            <p>{item.preco}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+    </section>
+    </>
   )
 }
 
-export default cards
+export default Cards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Cards from '../components/Cards'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Cards/>
   )
 }
 
