78a80014a366895712681372483d2cb83a759f79
janinynobrega <janinynobrega@gmail.com>
2025-05-16 11:15:16 -0300

    configuração da font inter, com instalação do @fontsource/inter

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/inter'; // Peso normal (400)
+import '@fontsource/inter/600.css'; // Peso semibold (opcional)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
