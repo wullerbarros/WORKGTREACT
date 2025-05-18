aa1ee33e50497faedd2bd036a2fd17dc46596549
Débora Schnornberger <schnornbergerdebora@gmail.com>
2025-05-18 14:27:28 -0300

    aside ultilizando filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import './App.css'
-import Rotas from './rotas/Rotas'
+import FilterGroup from './components/FilterGroup';
+import FilterPanel from './components/FilterGroup'
+import { useState } from 'react'
 
-function App() {
+
+const produtosExemplo = [
+  { nome: "Tênis A", marca: "Adiddas", categoria: "Esporte e lazer", genero: "Masculino", estado: "Novo" },
+  { nome: "Tênis B", marca: "Nike", categoria: "Casual", genero: "Feminino", estado: "Usado" },
+  // ...mais produtos
+];
+
+const App = () => {
+  const [filtrosAtivos, setFiltrosAtivos] = useState(null);
+
+  const produtosFiltrados = filtrosAtivos
+    ? produtosExemplo.filter((produto) =>
+        filtrosAtivos.marca.includes(produto.marca) &&
+        filtrosAtivos.categoria.includes(produto.categoria) &&
+        filtrosAtivos.genero.includes(produto.genero) &&
+        produto.estado === filtrosAtivos.estado
+      )
+    : produtosExemplo;
+
   return (
-    <>
-      <Rotas />
-    </>
-  )
-}
+    <div className="flex">
+      <FilterGroup onApply={setFiltrosAtivos} />
+      <div className="w-[308px] h-[720px] mt- [332px] left-[100px] rounded-sm gap-20">
+        <h1 className="text-xl font-bold mb-4 [#991956]">Produtos</h1>
+        <ul>
+          {produtosFiltrados.map((produto, idx) => (
+            <li key={idx} className="border p-2 mb-2 rounded">
+              {produto.nome} - {produto.marca} - {produto.categoria} - {produto.genero} - {produto.estado}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
 
 export default App
