7d4efc5034ec3a1ec54615c7d3f8eb15a25a6db9
janinynobrega <janinynobrega@gmail.com>
2025-05-22 13:32:34 -0300

    separação da coleçao em destaque

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import Header from '../components/Header'
 import CarouselJ from '../components/CarouselJ'
 import BodyJ from '../components/BodyJ'
 import Section from '../components/Section'
+import Destaque from '../components/Destaque'
 
 
 const Home = () => {
   return (
     <>
     <CarouselJ />
-    <Section />    
+    <Destaque />    
     </>    
   )
 }
