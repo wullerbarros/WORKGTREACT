b3df8b9e50564048d723a6a2ee821053424127c5
jonathas-88 <jonathas.sabino@gmail.com>
2025-05-27 17:15:54 -0300

    Cards

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Destaque from './Destaque'
-import Cards2 from './Cards2'
+import Cards from './Cards'
 import OfertaEspecial from './OfertaEspecial'
 
 const Section = () => {
   return (
     <>
     <Destaque />
-    <Cards2 quantidade={8} className="grid grid-cols-4 mr-[100px] ml-[100px]"/>
+    <Cards quantidade={8} className="grid grid-cols-4 mr-[100px] ml-[100px]"/>
     <OfertaEspecial />
     </>
   )
