f28fa6a70b2cfb43d1e981dbb6336bb4536f8eeb
janinynobrega <janinynobrega@gmail.com>
2025-05-28 14:04:32 -0300

    ajustes

--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -7,7 +7,7 @@ const ProductViewPage = () => {
   return (
     <>
       <ProductView />
-      <Cards quantidade={4} className="bg-[#F9F8FE] flex flex-col-1 ml-[100px] mr-[100px]"/>
+      <Cards quantidade={4} />
     </>
   );
 };
