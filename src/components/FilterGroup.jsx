d8d84d04a86dc706b71b8e844ba7a37fb211a93f
Débora Schnornberger <schnornbergerdebora@gmail.com>
2025-05-29 11:50:11 -0300

    ajustes do aside-usando tailwind

--- src/components/FilterGroup.jsx
+++ src/components/FilterGroup.jsx
@@ -37,16 +37,16 @@ const FilterGroup = ({ onApply }) => {
     <div className="pt-[120px] ml-[100px]">
       <div className="bg-[#FFFFFF] p-[30px] w-[308px] h-[720px]">
         <div className="flex justify-between items-center mb-2 font-bold text-[#474747]">
-          <h2 className="text-[16px] w-[85px] h-[24px] font-bold leading-[0.75px] !font-[inter] tracking-tight mb-4">Filtrar por</h2>
+          <h2 className="text-base w-[85px] h-[24px] text-[#474747] !font-bold !font-[inter] tracking-[0.75px] mb-4">Filtrar por</h2>
         </div>
-        <hr className="w-[248px] border border-[#CCCCCC]"/>
+        <hr className="w-[248px] border-[1px] content-center border-[#CCCCCC]"/>
 
         <FilterSection title="Marca" options={marcas} selected={filters.marca} onToggle={(val) => toggleCheckbox("marca", val)} />
         <FilterSection title="Categoria" options={categorias} selected={filters.categoria} onToggle={(val) => toggleCheckbox("categoria", val)} />
         <FilterSection title="Gênero" options={generos} selected={filters.genero} onToggle={(val) => toggleCheckbox("genero", val)} />
 
         <div className="mb-4 w-[76px] h-[86px]">
-          <h3 className="w-[52px] h-[22px] font-medium mb-1 gap-0.5">Estado</h3>
+          <h3 className="text-sm text-[#474747] !font-[inter] !font-bold w-[52px] h-[22px] mb-1 tracking-[0.75px]">Estado</h3>
           {estados.map((estado) => (
             <label key={estado} className="flex items-center space-x-2 mb-1">
               <input
@@ -57,12 +57,12 @@ const FilterGroup = ({ onApply }) => {
                 onChange={() => handleRadioChange(estado)}
                 className="accent-[#C92071] w-[22px] h-[14px] t-[4px] l-[4px] gap-0.5"
               />
-              <span>{estado}</span>
+              <span className="!font-[inter] tracking-[0.25px] text-sm text-[#474747] ml-[12px]">{estado}</span>
             </label>
           ))}
         </div>
 
-        <button onClick={applyFilters} className="bg-[#991956] text-white px-4 py-2 rounded-lg w-full mt-4">
+        <button onClick={applyFilters} className="bg-[#991956] !text-white px-4 py-2 rounded-lg w-full mt-4">
           Aplicar filtros
         </button>
       </div>
--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,16 +1,16 @@
 const FilterSection = ({ title, options, selected, onToggle }) => (
-  <div className="w-[136px] h-[150px] gap-2.5 mb-4 mt-6">
-    <h3 className="font-medium mb-1">{title}</h3>
+  <div className="w-[150px] h-[150px] gap-2.5 mb-4 mt-6">
+    <h3 className="mb-1 text-sm text-[#474747] !font-[inter] !font-bold tracking-[0.75px]">{title}</h3>
     {options.map((item) => (
       <label key={item} className="flex items-center space-x-2 mb-1">
         <input
           type="checkbox"
           checked={selected.includes(item)}
           onChange={() => onToggle(item)}
-          className="accent-[#C92071] w-[22px] h-[22px] rounded-xs border peer-hidden inner:peer-checked  t-[4px] l-[4px]"
+          className="accent-[#C92071] !font-[inter] w-[22px] h-[22px] rounded-xs border peer-hidden inner:peer-checked  t-[4px] l-[4px]"
           
         /> 
-        <span>{item}</span>
+        <span className="!font-[inter] tracking-[0.25px] text-sm text-[#474747] ml-[12px] gap-0.5 ">{item}</span>
       </label>
     ))}
   </div>
